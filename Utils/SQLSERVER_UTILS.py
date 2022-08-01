f108fc311d23d9d774fd53ca6c910b40a1ea6671
mahid721 <bigdatamd2521@gmail.com>
2022-08-01 08:59:09 +0530

    commit

---
+++ Utils/SQLSERVER_UTILS.py
@@ -0,0 +1,43 @@
+import pyodbc
+from datetime import datetime
+
+
+
+class mysql_connector:
+    def __init__(self, Driver, Server,Database, Trusted_Connection='yes', autocommit=True):
+        self.today = datetime.today()
+        self.conn = pyodbc.connect(Driver=Driver,
+                                   Server=Server,
+                                   Database=Database,
+                                   Trusted_Connection=Trusted_Connection,
+                                   autocommit=autocommit)
+        self.cursor_obj = self.conn.cursor()
+
+    def create_sql (self, file_path):
+        create_file = open(file_path, 'r')
+        sql_create = create_file.read()
+        for x in sql_create.split(';'):
+            print(x)
+            self.cursor_obj.execute(x)
+        create_file.close()
+
+    def insert_sql_raw(self, file_path_csv, file_path_sql):
+        insert_raw = open(file_path_csv, 'r')
+        data_insert_raw = insert_raw.readlines()
+        insert_raw.close()
+        insert_file = open(file_path_sql, 'r')
+        sql_insert_raw = insert_file.read()
+        insert_file.close()
+        for rows in data_insert_raw:
+            rows = rows.replace('\n', '')
+            self.cursor_obj.execute(sql_insert_raw, *rows.split(','), self.today)
+
+    def insert_sql_final(self, insert_sql_filepath):
+        insert_file = open(insert_sql_filepath, 'r')
+        sql_insert_final = insert_file.read()
+        self.cursor_obj.execute(sql_insert_final)
+        insert_file.close()
+
+
+
+
