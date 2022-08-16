435e69d9c0cbc833cf0c4c8d57da3cf4bf2f5df6
mahid721 <bigdatamd2521@gmail.com>
2022-08-16 19:59:16 +0530

    Added Logging & Exception handing

--- main_incremental.py
+++ main_incremental.py
@@ -1,9 +1,21 @@
+import logging
+from datetime import datetime
 
+from logs.log_file_name_create import log_file_name
 from utils.sqlserver_utils import mysql_connector
 import yaml
 from yaml.loader import SafeLoader
 import os
 from utils.check_data_files import expected_files, available_data_files, files_to_be_process, get_ts_from_path
+
+
+
+logging = log_file_name('C:\\Users\\Lenovo\\PycharmProjects\\Project_EDM\\logs\\', 'main_incremental')
+
+logging.error(" error - my name is mahesh")
+print("print - my name is mahesh")
+
+
 
 
 with open("conf/incremental_paths.yml", 'r') as f:
@@ -25,21 +37,23 @@ files_to_be_process_lst = files_to_be_process(available_data_files_lst, expected
 
 
 for file_full_path in files_to_be_process_lst:
-    if os.path.getsize(file_full_path) > 0:
-        variable_dict = get_ts_from_path(file_full_path)
-        for sql_file_path in data['SQLS']['DML']:
-            load = sql_file_path.split('\\')
-            if load[-2] == 'file_load':
-                print('FILE_LOAD :: Executing SQL file  :', load[-1], 'for load dated : ', variable_dict['date_time'])
-                print(file_full_path)
-                print(sql_file_path)
-                mysql_obj.insert_sql_file_load(file_full_path, sql_file_path)
-            elif load[-2] == 'table_load':
-                print('TABLE_LOAD :: Executing SQL file  :', load[-1], 'for load dated : ', variable_dict['date_time'])
-                mysql_obj.insert_sql_table_load(sql_file_path, variable_dict)
-            elif file_full_path == files_to_be_process_lst[-1]:
-                print('Updating last_processed_file_ts :: Executing SQL file  :', load[-1], 'for load dated : ', variable_dict['date_time'])
-                mysql_obj.update_sql_control_table(sql_file_path, variable_dict)
-            # time.sleep(30)
+    try:
+        if os.path.getsize(file_full_path) > 0:
+            variable_dict = get_ts_from_path(file_full_path)
+            for sql_file_path in data['SQLS']['DML']:
+                load = sql_file_path.split('\\')
+                if load[-2] == 'file_load':
+                    logging.info('FILE_LOAD :: Executing SQL file  :' + str(load[-1]) + 'for load dated : ' + str(variable_dict['date_time']))
+                    mysql_obj.insert_sql_file_load(file_full_path, sql_file_path)
+                elif load[-2] == 'table_load':
+                    logging.info('TABLE_LOAD :: Executing SQL file  :' + str(load[-1]) + 'for load dated : '  + str(variable_dict['date_time']))
+                    mysql_obj.insert_sql_table_load(sql_file_path, variable_dict)
+                elif file_full_path == files_to_be_process_lst[-1]:
+                    logging.info('Updating last_processed_file_ts :: Executing SQL file  :' + str(load[-1]) + 'for load dated : ' + str(variable_dict['date_time']))
+                    mysql_obj.update_sql_control_table(sql_file_path, variable_dict)
+                # time.sleep(30)
+    except Exception as e:
+        logging.error('Error Due to ' + str(e))
+        raise Exception(e)
 
 
--- utils/sqlserver_utils.py
+++ utils/sqlserver_utils.py
@@ -1,93 +1,89 @@
 import logging
-import sys
 import pyodbc
-from logs.log_file_name_create import log_file_name
-
-
-
-
-# log_file_name('C:\\Users\\Lenovo\\PycharmProjects\\Project_EDM\\logs\\', 'sqlserver_utils')
 
 class mysql_connector:
-    log_file_name('C:\\Users\\Lenovo\\PycharmProjects\\Project_EDM\\logs\\', 'sqlserver_utils')
-    try:
-        def __init__(self, driver, server, database, trusted_connection='yes', autocommit=True):
+    def __init__(self, driver, server, database, trusted_connection='yes', autocommit=True):
+        try:
             self.conn = pyodbc.connect(Driver=driver,
                                        Server=server,
                                        Database=database,
                                        Trusted_Connection=trusted_connection,
                                        autocommit=autocommit )
             self.cursor_obj = self.conn.cursor()
-    except Exception as e:
-        logging.info('Error Due to ', e)
-        sys.exit()
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
 
-    try:
-        def file_read_sql(self, file_path):
-            logging.info('This is info message -- file_read_sql -- ', file_path)
+
+    def file_read_sql(self, file_path):
+        try:
+            logging.info('This is info message -- file_read_sql -- ' + str(file_path))
             open_file = open(file_path, 'r')
             read_file = open_file.read()
             open_file.close()
             return read_file
-    except Exception as e:
-        logging.error('Error Due to ', e)
-        sys.exit()
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
 
-    try:
-        def file_read_csv(self, file_path):
+
+    def file_read_csv(self, file_path):
+        try:
+            logging.info('This is info message -- read_csv_file -- ' + str(file_path))
             open_file = open(file_path, 'r')
             read_file = open_file.readlines()
             open_file.close()
             return read_file
-    except Exception as e:
-        logging.error('Error Due to ', e)
-        sys.exit()
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
 
-    try:
-        def create_sql(self, file_path):
+
+    def create_sql(self, file_path):
+        try:
             sql_create = self.file_read_sql(file_path)
             for x in sql_create.split(';'):
-                logging.info('create sql file/query ', x)
+                logging.info('create sql file/query ' + str(x))
                 self.cursor_obj.execute(x)
-    except Exception as e:
-        logging.error('Error Due to ', e)
-        sys.exit()
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
 
-    try:
-        def insert_sql_file_load(self, file_path_csv, file_path_sql):
+
+    def insert_sql_file_load(self, file_path_csv, file_path_sql):
+        try:
             data_insert_raw = self.file_read_csv(file_path_csv)
-            logging.info('reading data/csv file', data_insert_raw)
+            logging.info('reading data/csv file' + str(data_insert_raw))
             sql_insert_temp = self.file_read_sql(file_path_sql)
-            logging.info('reading sql file/query', sql_insert_temp)
+            logging.info('reading sql file/query' + str(sql_insert_temp))
             for rows in data_insert_raw:
                 rows = rows.replace('\n', '')
-                print(rows)
-                print(sql_insert_temp, *rows.split(','))
-                print(rows.split(','))
-                print(type(rows.split(',')))
                 self.cursor_obj.execute(sql_insert_temp, *rows.split(','))
-                # logging.info('Inserting data into temp table from csv file', sql_insert_temp, *rows.split(','))
-    except Exception as e:
-        logging.error('Error Due to ', e)
-        sys.exit()
+                logging.info('Inserting data into temp table from csv file' + str(sql_insert_temp) + str(rows.split(',')))
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
 
-    try:
-        def insert_sql_table_load(self, insert_sql_filepath, variable_dict):
+    def insert_sql_table_load(self, insert_sql_filepath, variable_dict):
+        try:
             sql_insert_table_load = self.file_read_sql(insert_sql_filepath)
-            logging.info('reading sql file/query', sql_insert_table_load)
-            print(sql_insert_table_load.format(**variable_dict))
+            logging.info('reading sql file/query' + str(sql_insert_table_load))
             self.cursor_obj.execute(sql_insert_table_load.format(**variable_dict))
-            logging.info('Inserting data into  table', sql_insert_table_load.format(**variable_dict))
-    except Exception as e:
-        logging.error('Error Due to ', e)
-        sys.exit()
+            logging.info('Inserting data into  table' + str(sql_insert_table_load.format(**variable_dict)))
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
 
-    try:
-        def update_sql_control_table(self, update_sql_filepath, variable_dict):
+
+    def update_sql_control_table(self, update_sql_filepath, variable_dict):
+        try:
             sql_update_file = self.file_read_sql(update_sql_filepath)
-            logging.info('reading sql file/query', sql_update_file)
+            logging.info('reading sql file/query' + str(sql_update_file))
             self.cursor_obj.execute(sql_update_file.format(**variable_dict))
-            logging.info('Updating last processed file  into control table', sql_update_file.format(**variable_dict))
-    except Exception as e:
-        logging.error('Error Due to ', e)
-        sys.exit()
+            logging.info('Updating last processed file  into control table' + str(sql_update_file.format(**variable_dict)))
+        except Exception as e:
+            logging.error('Error Due to ' + str(e))
+            raise Exception(e)
+
+
+
